c13b454b3291b823a0b5644904bdcb585de23fbf
ipek <ipek_serttas99@htomail.com>
2022-01-13 13:46:11 +0300

    3. hafta ödevi 🔮

---
+++ Data(Homework)/Middleware/BlockMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Homework_.Middleware
+{
+    public class BlockMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public BlockMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke (HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/api/Vehicles/1")) //1 id'li vehicle'a gittiğinde 
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("403 -  3. odev kosulu saglandi...");
+                
+                return;
+        }
+
+        await next.Invoke(context);
+        }
+    }
+}
---
+++ TrashCollectionSystem/Startup.cs
@@ -0,0 +1,86 @@
+using AutoMapper;
+using Data_Homework_.Context;
+using Data_Homework_.Mapper;
+using Data_Homework_.Middleware;
+using Data_Homework_.Uow;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace TrashCollectionSystem
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //add context
+            var dbConfig = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<TrashSystemDbContext>(options =>
+            options.UseSqlServer("Server=IPEK; Database=trashcollectionsystem; Trusted_Connection=True;"));
+
+            
+            //add uow
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            //add automapper
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TrashCollectionSystem", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TrashCollectionSystem v1"));
+            }
+
+            app.UseMiddleware<BlockMiddleware>();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
